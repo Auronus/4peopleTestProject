e13460abae0fca3c7f265c1ef5987d6990180348
Погодин Константин <auronus@mail.ru>
2021-03-06 10:17:05 +0400

    init commit

---
+++ src/main/java/tests/BaseTest.java
@@ -0,0 +1,12 @@
+package tests;
+
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+
+/**
+ * Базовый класс отвечающий за создание драйвера
+ */
+public class BaseTest {
+
+
+
+}
---
+++ src/main/java/tests/search/SearchTest.java
@@ -0,0 +1,9 @@
+package tests.search;
+
+import tests.BaseTest;
+
+/**
+ * Класс для проверки работы поиска
+ */
+public class SearchTest extends BaseTest {
+}
